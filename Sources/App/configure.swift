42ae1e733bfd90a25c147dea60c7c0dd75f62621
A.C <caxche@gmail.com>
2018-05-18 10:47:08 +0800

    update for 3.0 GM

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,8 +1,6 @@
 import Vapor
 
 /// Called before your application initializes.
-///
-/// https://docs.vapor.codes/3.0/getting-started/structure/#configureswift
 public func configure(
     _ config: inout Config,
     _ env: inout Environment,
@@ -17,9 +15,7 @@ public func configure(
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
-    middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
 
 }
